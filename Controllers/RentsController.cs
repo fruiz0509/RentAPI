05db8c0657b9d1f75e15724aa15e3b6b9ff78ac4
Francisco Ruiz <fruiz_cruz@outlook.com>
2023-04-21 21:10:55 -0600

    Rent controller was created

---
+++ Controllers/RentsController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RentAPI.Models;
+using RentAPI.Repository;
+
+namespace RentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RentsController : ControllerBase
+    {
+        private readonly IRepository<Rent> _rentRepository;
+        public RentsController(IRepository<Rent> rentRepository)
+        {
+            _rentRepository = rentRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get() 
+        {
+            try
+            {
+                return Ok(new { message = "OK", response = _rentRepository.Get() });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new { message = ex.Message });
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                Rent rent = _rentRepository.Get(id);
+                if(rent == null)
+                    return NotFound();
+
+                return Ok(new { message = "OK", response = rent });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new { message = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Rent rent) 
+        {
+            try
+            {
+                _rentRepository.Add(rent);
+                _rentRepository.Save();
+                return Ok(new { message = "OK" });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new { message = ex.Message });
+            }
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Rent rent)
+        {
+            try
+            {
+                _rentRepository.Update(rent);
+                _rentRepository.Save();
+                return Ok(new { message = "OK" });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new { message = ex.Message });
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) 
+        {
+            try
+            {
+                _rentRepository.Delete(id);
+                _rentRepository.Save();
+                return Ok(new { message = "OK" });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new { message = ex.Message });
+            }
+        }
+    }
+}
